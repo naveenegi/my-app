9e017546a7cd9aec7686b73819eca498b182628c
github_naveenegi <naveenegi@gmail.com>
2020-08-26 16:43:40 +0530

    Updates

---
+++ precache-manifest.f0a300705cdb1c422960045a2169ca49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7c1f857255c657dc1ed6bb90f9edc7",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ee50a6ec1c349c400580",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6f2482942a5b659e47d",
+    "url": "/my-app/static/js/2.8b7e9912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.8b7e9912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee50a6ec1c349c400580",
+    "url": "/my-app/static/js/main.d19bb0ea.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
